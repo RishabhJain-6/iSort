51792a85a72e22d41fdceece9c61d6b260090f0e
Rishabh Jain <rishabhjain61002@gmail.com>
2023-03-30 15:21:49 +0530

    Update bubbleSort.js

--- javascript/bubbleSort.js
+++ javascript/bubbleSort.js
@@ -50,19 +50,103 @@ generatebars(25);
 function generate() {
 	var n = document.getElementById("nele");
 	var numele = parseInt(n.value);
-	if (numele>400) {
-        	window.alert("Upper bound is 400 bars. Kindly choose a value in that range!");
-       		n.value=400;
-        	generate();
-    	}
-	else {
-		generatebars(numele);
-	}
+    if (numele>400) {
+        window.alert("Upper bound is 500 bars. Kindly choose a value in that range!");
+        n.value=400;
+        generate();
+    }
+    else {
+        generatebars(numele);
+    }
 }
 
 function generate2() {
 	const value = Math.floor(Math.random() * 80) + 1;
 	generatebars(value);
+}
+
+function generatebarsWorst(num) {
+    container.innerHTML=""
+    let values = Array.from({length: num}, (_, i) => Math.max(num - i + 9, 10));
+    for (let i = 0; i < num; i += 1) {
+        const value = values[i];
+	    const bar = document.createElement("div");
+
+	    bar.classList.add("bar");
+	    bar.style.height = `${value/2}%`;
+
+		const barLabel = document.createElement("label");
+		barLabel.classList.add("bar__id");
+	    barLabel.innerHTML = value;
+
+        if (num>80) {
+            barLabel.style.display='none';
+        }
+
+		if (num<=40) {
+			if (num<=10) {
+				barLabel.style.fontSize = 'xxx-large';
+			}
+            else if (num<=20) {
+                barLabel.style.fontSize = 'xx-large';
+            }
+            if (num>20 && num<=30) {
+				barLabel.style.fontSize = 'x-large';
+			}
+            else if (num<=40) {
+                barLabel.style.fontSize = 'large';
+            }
+		}
+		bar.appendChild(barLabel);
+	    container.appendChild(bar);
+    }
+}
+
+function generatebarsBest(num) {
+    container.innerHTML=""
+    let values = Array.from({length: num}, (_, i) => i + 1 + 9);
+    for (let i = 0; i < num; i += 1) {
+        const value = values[i];
+	    const bar = document.createElement("div");
+
+	    bar.classList.add("bar");
+	    bar.style.height = `${value/2}%`;
+
+		const barLabel = document.createElement("label");
+		barLabel.classList.add("bar__id");
+	    barLabel.innerHTML = value;
+
+        if (num>80) {
+            barLabel.style.display='none';
+        }
+
+		if (num<=40) {
+			if (num<=10) {
+				barLabel.style.fontSize = 'xxx-large';
+			}
+            else if (num<=20) {
+                barLabel.style.fontSize = 'xx-large';
+            }
+            if (num>20 && num<=30) {
+				barLabel.style.fontSize = 'x-large';
+			}
+            else if (num<=40) {
+                barLabel.style.fontSize = 'large';
+            }
+		}
+		bar.appendChild(barLabel);
+	    container.appendChild(bar);
+    }
+}
+
+function generatebest() {
+    const value = Math.floor(Math.random() * 80) + 1;
+    generatebarsBest(value);
+}
+
+function generateworst() {
+    const value = Math.floor(Math.random() * 80) + 1;
+    generatebarsWorst(value);
 }
 
 function disable() {
@@ -75,6 +159,12 @@ function disable() {
 
     document.getElementById("Button3").disabled = true;
     document.getElementById("Button3").style.backgroundColor = "#d8b6ff";
+
+    document.getElementById("Button4").disabled = true;
+    document.getElementById("Button4").style.backgroundColor = "#d8b6ff";
+
+    document.getElementById("Button5").disabled = true;
+    document.getElementById("Button5").style.backgroundColor = "#d8b6ff";
 }
 
 var delay = 5000;
@@ -111,6 +201,12 @@ async function BubbleSort() {
     
                     document.getElementById("Button3").disabled = false;
                     document.getElementById("Button3").style.backgroundColor = "#a54997";
+
+                    document.getElementById("Button4").disabled = false;
+                    document.getElementById("Button4").style.backgroundColor = "#a54997";
+
+                    document.getElementById("Button5").disabled = false;
+                    document.getElementById("Button5").style.backgroundColor = "#a54997";
     
                     isPlaying = false;
                     pausePlayBtn.textContent = 'Pause';
@@ -137,10 +233,16 @@ async function BubbleSort() {
                 document.getElementById("Button3").disabled = false;
                 document.getElementById("Button3").style.backgroundColor = "#a54997";
 
+                document.getElementById("Button4").disabled = false;
+                document.getElementById("Button4").style.backgroundColor = "#a54997";
+
+                document.getElementById("Button5").disabled = false;
+                document.getElementById("Button5").style.backgroundColor = "#a54997";
+
                 for (let k=0;k<bars.length;k++) {
                     bars[k].style.backgroundColor="rgb(236, 190, 53)";
                 }
-                
+
                 terminate = !terminate;
                 return;
             }
@@ -215,6 +317,12 @@ async function BubbleSort() {
 
     document.getElementById("Button3").disabled = false;
     document.getElementById("Button3").style.backgroundColor = "#a54997";
+
+    document.getElementById("Button4").disabled = false;
+    document.getElementById("Button4").style.backgroundColor = "#a54997";
+
+    document.getElementById("Button5").disabled = false;
+    document.getElementById("Button5").style.backgroundColor = "#a54997";
 
     compare.textContent=' ' + " No of Comparisons: "+count;
     
